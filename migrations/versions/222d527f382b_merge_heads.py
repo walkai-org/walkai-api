49c5df64b64992c7aa93acf6c445a88956338394
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2025-12-13 16:45:03 -0300

    add merge heads migration

---
+++ migrations/versions/222d527f382b_merge_heads.py
@@ -0,0 +1,20 @@
+"""merge heads
+
+Revision ID: 222d527f382b
+Revises: 91462034ac11, c48b5adf4c2a
+Create Date: 2025-12-13 16:44:28.031283
+"""
+
+# revision identifiers, used by Alembic.
+revision = "222d527f382b"
+down_revision = ("91462034ac11", "c48b5adf4c2a")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
